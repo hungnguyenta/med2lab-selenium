2b8587916e1afc112310c3c5bb716dcc8c27683c
hung.nguyen <hung.nguyen>
2021-12-18 18:17:48 +0700

    clean up files

--- sendEmail.py
+++ sendEmail.py
@@ -5,7 +5,7 @@ from sendgrid.helpers.mail import Mail, Email, To, Content
 def sendEmail():
     sg = sendgrid.SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
     from_email = Email("hung.nguyen@med2lab.com")  # Change to your verified sender
-    to_email = To("hung.nguyen@med2lab.com")  # Change to your recipient
+    to_email = To("hung.nguyen@med2lab.com;hungnm.nazzy@gmail.com")  # Change to your recipient
     subject = "Med2lab-SELENIUM Error script testing"
     content = Content("text/plain", "Hey, please take a look the scripts testing. We've got some error. Could you try at local to fix then upload the script to retry")
     mail = Mail(from_email, to_email, subject, content)
