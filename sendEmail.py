dd0f0d742d6c681a55443a4f240f8a5d964a18af
hung.nguyen <hung.nguyen>
2021-12-18 18:37:35 +0700

    send multiple recipients

--- sendEmail.py
+++ sendEmail.py
@@ -4,17 +4,18 @@ from sendgrid.helpers.mail import Mail, Email, To, Content, Personalization
 
 def sendEmail():
     sg = sendgrid.SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
+
+
     from_email = Email("hung.nguyen@med2lab.com")  # Change to your verified sender
-    person1 = Personalization()
-    person1.add_to(Email ("hung.nguyen@med2lab.com"))
-    person2 = Personalization()
-    person2.add_to(Email ("hungnm.nazzy@gmail.com"))
+    # Replace these with your email addresses and names
+    to_emails = [
+        ('hung.nguyen@med2lab.com', 'Steven Universe'),
+        ('Hungnm.nazzy@gmail.com', 'Garnet')
+    ]
 
     subject = "Med2lab-SELENIUM Error script testing"
     content = Content("text/plain", "Hey, please take a look the scripts testing. We've got some error. Could you try at local to fix then upload the script to retry")
-    mail = Mail(from_email, None, subject, content)
-    mail.add_personalization(person1)
-    mail.add_personalization(person2)
+    mail = Mail(from_email,to_emails=to_emails,is_multiple=True, subject= subject,content = content)
 
     # Get a JSON-ready representation of the Mail object
     mail_json = mail.get()
