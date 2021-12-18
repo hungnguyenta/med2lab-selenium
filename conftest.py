90410d7935dc4cb30abecdff3c07a8b076b893c7
hung.nguyen <hung.nguyen>
2021-12-18 18:13:37 +0700

    send email if pytest error

--- conftest.py
+++ conftest.py
@@ -4,6 +4,6 @@ from sendEmail import sendEmail
 def pytest_terminal_summary(terminalreporter, exitstatus):
     if(exitstatus == pytest.ExitCode.OK):
         print("TEST OK")
-        sendEmail()
     else:
         print("SEND EMAIL HERE")
+        sendEmail()
--- sendEmail.py
+++ sendEmail.py
@@ -2,11 +2,7 @@ import sendgrid
 import os
 from sendgrid.helpers.mail import Mail, Email, To, Content
 
-def demo():
-    print('demo at here')
-
 def sendEmail():
-    print(os.environ.get('SENDGRID_API_KEY'))
     sg = sendgrid.SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
     from_email = Email("hung.nguyen@med2lab.com")  # Change to your verified sender
     to_email = To("hung.nguyen@med2lab.com")  # Change to your recipient
@@ -20,3 +16,4 @@ def sendEmail():
     # Send an HTTP POST request to /mail/send
     response = sg.client.mail.send.post(request_body=mail_json)
     print(response.status_code)
+    print(response.headers)
