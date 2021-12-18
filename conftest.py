60f5f29a6876d762fabab18d35754f31ef9e8ead
hung.nguyen <hung.nguyen>
2021-12-18 17:53:49 +0700

    send email

--- conftest.py
+++ conftest.py
@@ -1,7 +1,9 @@
 import pytest
+from sendEmail import sendEmail
+
 def pytest_terminal_summary(terminalreporter, exitstatus):
     if(exitstatus == pytest.ExitCode.OK):
         print("TEST OK")
-        # sendEmail()
+        sendEmail()
     else:
         print("SEND EMAIL HERE")
