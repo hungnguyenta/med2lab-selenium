80a80f021403dd95e296574eb5a130724967875b
hung.nguyen <hung.nguyen@watasolutions.com>
2021-12-18 20:04:05 +0700

    add test demo

--- conftest.py
+++ conftest.py
@@ -1,9 +1,10 @@
 import pytest
 from sendEmail import sendEmail
 
+
 def pytest_terminal_summary(terminalreporter, exitstatus):
     if(exitstatus == pytest.ExitCode.OK):
         print("TEST OK")
     else:
         print("SEND EMAIL HERE")
-        sendEmail()
+        # sendEmail()
